4e04e867988c7360bbd1407172aec85dd63edf7b
Administrator <qx.ouyang@gmail.com>
2011-05-24 14:00:25 +0800

    Change-Id: I02e5fe4cb5364ad4040d10c22799fa06d778c081 Signed-off-by: Administrator <qx.ouyang@gmail.com>

--- src/JTextAreaTest.java
+++ src/JTextAreaTest.java
@@ -7,7 +7,6 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.UnsupportedEncodingException;
